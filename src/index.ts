6c74098236c8f5523e85a434b466c7cda806914d
ignacio-r <ignaciorobledounq@gmail.com>
2022-04-08 02:24:55 -0300

    add a subscriber

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
-import { Observable } from 'rxjs'
+import { Observable, Observer } from 'rxjs'
+
+const observer: Observer<string> = {
+    next: (v) => console.log(v),
+    error: (e) => console.warn(e),
+    complete: () => console.info('complete!') 
+}
 
 const obs$ = new Observable<string>(subs => {
     subs.next('Hola')
     subs.next('Mundo')
+
+    const a = undefined
+    a.nombre = 'ignacio'
     subs.complete();
 });
 
-obs$.subscribe(console.log)
+obs$.subscribe(
+    observer
+)
 
 
 
